fc8eacb43c19d872b86d2d67bb73bd82d24ac0fa
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-04-24 14:03:36 +0200

    mdi font installed, add Tree component.

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,18 @@
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <Tree/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Tree from './components/Tree'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tree
   },
   data () {
     return {
---
+++ src/components/Tree.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <v-treeview
+    v-model="tree"
+    :open.sync="open"
+    :items="items"
+    :load-children="fetchSubFolder"
+    activatable
+    item-key="name"
+    open-on-click
+  >
+    <template v-slot:prepend="{ item, open }">
+      <v-icon v-if="!item.file">
+        {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+      </v-icon>
+      <v-icon v-else>
+        {{ files[item.file] }}
+      </v-icon>
+    </template>
+  </v-treeview>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    data: () => ({
+      open: [],
+      files: {
+        html: 'mdi-language-html5',
+        pdf: 'mdi-file-pdf',
+        txt: 'mdi-file-document-outline',
+        xls: 'mdi-file-excel'
+      },
+      tree: [],
+      items: []
+    }),
+    mounted () {
+      this.getFolders()
+    },
+    methods: {
+      async getFolders () {
+        const folders = await fetch('http://localhost:56665/api/Login/getSubFoldersList', 
+        {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({searchPath: "/content/*"})
+        })
+        const json = await folders.json()
+        this.items = json.map(folder => {
+          return {name: folder.name, children:[], parent: folder.parent}
+        })
+      }
+    }
+  }
+  
+</script>
+
+<style>
+
+</style>
