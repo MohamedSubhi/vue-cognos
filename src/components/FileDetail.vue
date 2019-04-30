aa6be66cd430e755f8b4c509906eec33e07c0dba
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-04-30 11:11:12 +0200

    nopramsmodal component added

--- src/components/FileDetail.vue
+++ src/components/FileDetail.vue
@@ -8,36 +8,27 @@
         >Select a Report</div>
         <v-card v-else :key="selected.name" class="pt-4 mx-auto" flat max-width="800">
             <v-card-text>
-            <h3 class="headline mb-2 blue--text">{{ selected.name }}</h3>
-            <div class="body-1 font-weight-bold text-md-left">Search path: <span class="caption">{{ selected.searchPath }}</span></div>
-            <div class="body-1 font-weight-bold text-md-left">Creation time: <span class="caption">{{ selected.creationTime }}</span></div>
-            <div class="body-1 font-weight-bold text-md-left">Modification time: <span class="caption">{{ selected.modificationTime }}</span></div>
-            <v-radio-group v-model="radioGroup" row>
-                <div class="body-1 font-weight-bold text-md-left">Output: </div>
-                <v-radio
-                label="PDF"
-                value="pdf"
-                ></v-radio>
-                <v-radio
-                label="Excel"
-                value="excel"
-                ></v-radio>
-            </v-radio-group>
+                <h3 class="headline mb-2 blue--text">{{ selected.name }}</h3>
+                <div class="body-1 font-weight-bold text-md-left">Search path: <span class="caption">{{ selected.searchPath }}</span></div>
+                <div class="body-1 font-weight-bold text-md-left">Creation time: <span class="caption">{{ selected.creationTime }}</span></div>
+                <div class="body-1 font-weight-bold text-md-left">Modification time: <span class="caption">{{ selected.modificationTime }}</span></div>
             </v-card-text>
             <v-divider></v-divider>
-            <v-btn color="success">Send per Email</v-btn>
+            <NoParamsModal />
         </v-card>
         </v-scroll-y-transition>
     </v-flex>
 </template>
 
 <script>
+import NoParamsModal from './NoParamsModal';
+
 export default {
+    components: {
+        NoParamsModal
+    },
     props: {
         selected: null
-    },
-    data: () => ({
-        radioGroup: "pdf",
-    })
+    }
 };
 </script>
---
+++ src/components/NoParamsModal.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="success" v-on="on" dark>Send per Email</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Report Options</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+                <v-flex xs12>
+                    <v-text-field label="Report Name"></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                    <v-text-field label="Email*" required></v-text-field>
+                </v-flex>
+                <v-radio-group v-model="radioGroup" row>
+                    <div class="body-1 font-weight-bold text-md-left">Output: </div>
+                    <v-radio
+                    label="PDF"
+                    value="pdf"
+                    ></v-radio>
+                    <v-radio
+                    label="Excel"
+                    value="excel"
+                    ></v-radio>
+                </v-radio-group>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog = false">Send</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        radioGroup: "pdf",
+        dialog: false
+    })
+  }
+</script>
